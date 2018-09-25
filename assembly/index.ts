7bbb3fa1a68bd76f06459dbaab247c4e04a924a0
Aron Homberg <info@aron-homberg.de>
2018-09-25 11:52:54 +0200

    Added some explanation

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,5 +6,11 @@ export function sum(arr: Int32Array): i32 {
     for (let i = 0, k = arr.length; i < k; ++i) {
         arr[i] += arr[i];
     }
+
+    // please note that this will return the latest value
+    // not the TypedArray or the memory location ptr ;)
+    // You can use the mutated TypedArray data in JS without
+    // returning anything here... in fact, the value returned
+    // here is ignored in JS context in this example (index.js)
     return v;
 }
